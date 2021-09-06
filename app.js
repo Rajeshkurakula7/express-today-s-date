4af6fac2dbaf29d71d36d3991692436a58da7272
Rajeshkurakula7 <kurakularajesh7@gmail.com>
2021-09-06 03:08:04 +0000

    Today's date displayed

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let todayDate = new Date();
+  response.send(
+    `${todayDate.getDate()}-${
+      todayDate.getMonth() + 1
+    }-${todayDate.getFullYear()}`
+  );
+});
+app.listen(3000);
+
+module.exports = app;
